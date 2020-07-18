e5a51702812bbd3f8caa1c67855158fd6fd968e9
Workflooow <administrator@kunnskapstrening.no>
2020-07-18 20:09:38 +0200

    first iteration

--- PokemonFusionFirstApproximation/OwnedPokemon.cs
+++ PokemonFusionFirstApproximation/OwnedPokemon.cs
@@ -28,50 +28,58 @@ namespace PokemonFusionFirstApproximation
 
             /*Dictionary<RedLib.PokemonTypes, int> typescores = new Dictionary<RedLib.PokemonTypes, int>();
             RedLib.PokemonTypes type1 = dexBase.headComponent.types.Item1;
-            RedLib.PokemonTypes type2 = dexBase.headComponent.types.Item2;
-            typescores.Add(dexBase.headComponent.types.Item1, 2);
-            if (dexBase.headComponent.types.Item2 != RedLib.PokemonTypes.typeless)
+            RedLib.PokemonTypes type2 = (dexBase.fused ? dexBase.bodyComponent.types.Item2 : dexBase.headComponent.types.Item2);
+            if (dexBase.fused)
             {
-                typescores.Add(dexBase.headComponent.types.Item2, 1);
-            }
-           
-            if (typescores.ContainsKey(dexBase.bodyComponent.types.Item1))
-            {
-                typescores[dexBase.bodyComponent.types.Item1]++;
-            }
-            else
-            {
-                typescores.Add(dexBase.bodyComponent.types.Item1, 1);
-            }
-
-            if (dexBase.bodyComponent.types.Item2 != RedLib.PokemonTypes.typeless && 
-                typescores.ContainsKey(dexBase.bodyComponent.types.Item2))
-            {
-                typescores[dexBase.bodyComponent.types.Item2]++;
-            }
-            else
-            {
-                typescores.Add(dexBase.bodyComponent.types.Item1, 2);
-            }
-
-            foreach (var item in typescores)
-            {
-                if (item.Value > typescores[dexBase.headComponent.types.Item1])
+                typescores.Add(dexBase.headComponent.types.Item1, 2);
+                if (dexBase.headComponent.types.Item2 != RedLib.PokemonTypes.typeless)
                 {
-                    type1 = item.Key;
+                    typescores.Add(dexBase.headComponent.types.Item2, 1);
                 }
-                if (item.Value > typescores[dexBase.bodyComponent.types.Item2])
+
+                if (typescores.ContainsKey(dexBase.bodyComponent.types.Item1))
                 {
-                    type2 = item.Key;
+                    typescores[dexBase.bodyComponent.types.Item1]++;
+                }
+                else
+                {
+                    typescores.Add(dexBase.bodyComponent.types.Item1, 1);
+                }
+
+                if (dexBase.bodyComponent.types.Item2 != RedLib.PokemonTypes.typeless &&
+                    typescores.ContainsKey(dexBase.bodyComponent.types.Item2))
+                {
+                    typescores[dexBase.bodyComponent.types.Item2]++;
+                }
+                else
+                {
+                    typescores.Add(dexBase.bodyComponent.types.Item2, 2);
+                }
+
+                foreach (var item in typescores)
+                {
+                    if (item.Value > typescores[dexBase.headComponent.types.Item1])
+                    {
+                        type1 = item.Key;
+                    }
+                    if (item.Value > typescores[dexBase.bodyComponent.types.Item2])
+                    {
+                        type2 = item.Key;
+                    }
                 }
             }
+            
 
             return new Tuple<RedLib.PokemonTypes, RedLib.PokemonTypes>(type1, type2);*/
 
             // traditional priority-based typing system
 
             RedLib.PokemonTypes type1 = dexBase.headComponent.types.Item1;
-            RedLib.PokemonTypes type2 = dexBase.bodyComponent.types.Item2;
+            RedLib.PokemonTypes type2 = dexBase.fused ?
+                                       (dexBase.bodyComponent.types.Item2 == RedLib.PokemonTypes.typeless ?
+                                        dexBase.bodyComponent.types.Item1 :
+                                        dexBase.bodyComponent.types.Item2):
+                                        dexBase.headComponent.types.Item2;
 
             return new Tuple<RedLib.PokemonTypes, RedLib.PokemonTypes>(type1, type2);
         }
